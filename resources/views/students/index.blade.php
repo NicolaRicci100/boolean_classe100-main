ed541271cd770f9815a99448aa3c38b9d3d95dbe
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-30 14:40:02 +0200

    index card fix

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,15 +7,14 @@
     </div>
     <section class="row row-cols-3">
         @foreach ($students as $student)
-            <div class="card col" style="width: 18rem;">
+            <div class="card col m-2" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">{{ $student->first_name }} {{ $student->last_name }}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary">Età:</strong> {{ $student->age }}</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias facilis odit illo
+                        provident quidem error consectetur</p>
                     <a class="btn btn-outline-success" href="{{ route('students.edit', $student) }}">Modifica</a>
-                    <form method="POST" action="{{ route('students.destroy', $student) }}">
+                    <form class="d-inline ms-2" method="POST" action="{{ route('students.destroy', $student) }}">
                         @method('DELETE')
                         @csrf
                         <button class="btn btn-outline-danger">Distruggi</button>
