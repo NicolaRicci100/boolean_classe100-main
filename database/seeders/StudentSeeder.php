72aa39d151e8d1df1a98c7c1df6834daa8362260
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-30 11:29:21 +0200

    DB compilation

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class StudentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newStudent = new Student();
+            $newStudent->first_name = $faker->firstName();
+            $newStudent->last_name = $faker->lastName();
+            $newStudent->age = rand(6, 30);
+            $newStudent->save();
+        }
     }
 }
