754c534a2377aeac76be1554a4f75760bc6a89ef
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-30 15:52:48 +0200

    aggiunto SoftDeletes al modello Students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,17 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
 
-    use HasFactory;
+  use HasFactory;
+  use SoftDeletes;
 
-    public function getFullName()
-    {
-        return $this->first_name . ' ' . $this->last_name;
-    }
+  public function getFullName()
+  {
+    return $this->first_name . ' ' . $this->last_name;
+  }
 
   protected $fillable = ['first_name', 'last_name', 'age'];
-
 }
